9b6c0774abb9c0a5bef69f9998ef213da7fa836a
Marco <marcovelardejr@gmail.com>
2022-05-16 23:54:43 -0500

    commit tree 5ed6ace1158ae7dfdecbe1839e8674070a73c864

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", () => {
+    let t = document.getElementById('text')
+    t.textContent = "This is really cool!"
+});
+
+
+
+
+
+
